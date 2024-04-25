4afb9a5c98b80b8aecd6c7e4c8596704cf2b7b5d
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-25 10:10:04 +0100

    Added search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,24 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  const [inputValue, setInputValue] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${inputValue} `);
+  }
+
+  function handleSearch(input) {
+    setInputValue(input.target.value);
+  }
+
   return (
     <div className="Dictionary">
       <h1>Dictionary</h1>
       <h3>What word would you like to know more about?</h3>
+      <form onSubmit={search}>
+        <input type="search" placeholder="Search" onChange={handleSearch} />
+      </form>
     </div>
   );
 }
