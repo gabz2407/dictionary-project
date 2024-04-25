a0019821fc69c43d8ca2a2784abee9e65b7a7bfd
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-25 10:09:39 +0100

    Project cleaning + added Dictionary component

---
+++ src/Dictionary.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+      <h3>What word would you like to know more about?</h3>
+    </div>
+  );
+}
